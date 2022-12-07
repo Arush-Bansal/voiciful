a162de4ca2405001170dad982cc0a99e09d6fd34
Arush Bansal <arush4bansal@gmail.com>
2022-12-07 17:25:50 +0530

    fixed space logic, need to fix next voice logic to middle of word

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@ let spokenWord = new SpeechSynthesisUtterance(); // the object difining the word
 var wordindex = 0; // index of the word that will be spoken next
 var words = "there is a god. And he wants to kill Vinayak. Believe me, its True."; // all the words that will be spoken
 var words_arr = words.split(" ");
-
+var letter_count = 0;
 
 // sets the basic speech properties.
 spokenWord.lang = "en";
@@ -28,15 +28,15 @@ let newInput = () => {
     // if (wordindex >= words_arr.length) {
     //     return;
     // }
+    letter_count += 1;
     if (wordindex >= words_arr.length) {
         wordindex = 0;
     }
-    if ((typingbox.value.slice(-1) === " ") && (typingbox.value.slice(-2) !== " ")) {
-        speak()
+    if ((typingbox.value.slice(-1) === " ") && ((typingbox.value.slice(-2, -1) !== " ") && (typingbox.value.slice(-2, -1) !== ""))) {
+        speak();
     }
 }
 
 typingbox.addEventListener("click", speak)
-typingbox.addEventListener("click", newInput)
 
 typingbox.addEventListener("input", newInput)
