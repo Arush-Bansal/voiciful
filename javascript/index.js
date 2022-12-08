64bc8d73570e50f0866c3208ec3310dcf7886838
Arush Bansal <arush4bansal@gmail.com>
2022-12-08 11:18:48 +0530

    middle random commit

--- javascript/index.js
+++ javascript/index.js
@@ -4,10 +4,9 @@ let typingbox = document.querySelector("#typingbox") // the box in which the use
 let spokenWord = new SpeechSynthesisUtterance(); // the object difining the word that is being spoken
 
 // word selector
-var wordindex = 0; // index of the word that will be spoken next
-var words = "there is a god. And he wants to kill Vinayak. Believe me, its True."; // all the words that will be spoken
+let wordindex = 0; // index of the word that will be spoken next
+let words = "Wikipedia is a free online encyclopedia, created and edited by volunteers around the world and hosted by the Wikimedia Foundation."; // all the words that will be spoken
 var words_arr = words.split(" ");
-var letter_count = 0;
 
 // sets the basic speech properties.
 spokenWord.lang = "en";
@@ -16,27 +15,29 @@ spokenWord.voice = window.speechSynthesis.getVoices()[0]
 spokenWord.rate = 1; // WORK: later change the rate of change of speed based on the typing speed.
 // spokenWord.volume = 10; // WORK: later add a volume button
 // spokenWord.pitch = 2;
-// console.log(spokenWord.voice)
-// addEventListener()
-let speak = () => {
+
+let speak = (wordindex) => {
     spokenWord.text = words_arr[wordindex];
     speaker.speak(spokenWord);
     wordindex += 1;
+    return wordindex;
 }
-let newInput = () => {
-    // if (value === undefined)
-    // if (wordindex >= words_arr.length) {
-    //     return;
-    // }
-    letter_count += 1;
+let newInput = (wordindex) => {
     if (wordindex >= words_arr.length) {
-        wordindex = 0;
+        wordindex = 0; // WORK : later change this with end slide(the corections, the speed and other stats)
     }
     if ((typingbox.value.slice(-1) === " ") && ((typingbox.value.slice(-2, -1) !== " ") && (typingbox.value.slice(-2, -1) !== ""))) {
-        speak();
+        wordindex = speak(wordindex);
     }
+    return wordindex;
 }
 
-typingbox.addEventListener("click", speak)
+typingbox.addEventListener("click", (wordindex) => {
+    speak(wordindex);
+    wordindex += 1;
+    speak(wordindex);
+    wordindex += 1;
+    return wordindex;
+}, wordindex)
 
-typingbox.addEventListener("input", newInput)
+typingbox.addEventListener("input", newInput, wordindex)
