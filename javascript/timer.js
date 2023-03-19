3b5c59357991401bbab15aaa58c7e37ce2fb9cab
Arush Bansal <arush4bansal@gmail.com>
2023-03-19 13:51:43 +0530

    show user text

--- javascript/timer.js
+++ javascript/timer.js
@@ -1,12 +1,21 @@
 const delay = (milli_sec) => new Promise((res,rej) => setTimeout(res, milli_sec))
 
 async function reset_words(){
-    await delay(1000);
-    let user_input = document.querySelector("textarea").value;
-    console.log(user_input);
-    document.querySelector('textarea').value = ""
-    // console.log("one second passed")
+    let delay_seconds = 5;
+    await delay(delay_seconds * 1000);
+    let user_input = document.querySelector("#typingbox").value;
+    display_user_words(user_input)
+    reset_text_area();
     reset_words()
 }
 
 reset_words();
+
+
+function display_user_words(text){
+    let previous_text = document.querySelector("#display-user-text");
+    previous_text.textContent = text;
+}
+function reset_text_area(){
+    document.querySelector('textarea').value = "";
+}
